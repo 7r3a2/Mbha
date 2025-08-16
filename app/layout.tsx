13d30142d610a420ef4ce264db953890b61696bd
Haider alaa ghadhban <m021094@uomustansiriyah.edu.iq>
2025-08-16 23:00:21 +0300

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,16 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="MBHA" />
+        <meta name="application-name" content="MBHA" />
+        <meta name="msapplication-TileColor" content="#3A8431" />
+        <meta name="theme-color" content="#3A8431" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/images/logo app.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/logo app.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo app.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo app.png" />
+        <link rel="mask-icon" href="/images/logo app.png" color="#3A8431" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
