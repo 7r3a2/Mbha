c44350a2ce84f3e72754cff020aa0ec96a991a95
Haider alaa ghadhban <m021094@uomustansiriyah.edu.iq>
2025-08-26 01:56:03 +0300

    new

--- app/qbank/page.tsx
+++ app/qbank/page.tsx
@@ -1922,7 +1922,7 @@ export default function Qbank() {
                         onChange={(e) => handleQuestionChange(Number(e.target.value))}
                         placeholder="1"
                       />
-                                             <span className="ml-2 text-sm text-gray-500">Required (1-100)</span>
+                                             <span className="ml-2 text-sm text-gray-500">Max Allowed 100</span>
                       <svg className="ml-1 text-gray-400 text-sm w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
